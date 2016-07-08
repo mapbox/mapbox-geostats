a027acfd1087452d7e69a686a05ce5bba2be8166
David Clark <dclark@mapbox.com>
2016-07-08 11:36:10 -0700

    More WIP

--- index.js
+++ index.js
@@ -1,27 +1,19 @@
 var path = require('path');
 var Promise = require('pinkie-promise');
-var gdal = require('./lib/gdal-stats');
+var gdalAnalyze = require('./lib/gdal-analyze');
+var Stats = require('./lib/stats');
 
 function buildGeoStats(filePath) {
-  var stats = {
-    count: 0,
-    fields: {},
-    geometryTypes: {
-      Unknown: 0,
-      Point: 0,
-      LineString: 0,
-      Polygon: 0,
-    },
-  };
+  return new Promise(function () {
+    var stats = new Stats(path.basename(filePath, path.extname(filePath)));
 
-  return new Promise(function() {
     if (!filePath) throw new Error('Filename required');
 
     var extension = path.extname(filePath);
 
     switch (extension) {
       case '.geojson':
-        return gdal(stats, filePath);
+        return gdalAnalyze(stats, filePath);
       default:
         throw new Error('Invalid file type');
     }
--- test/test.js
+++ test/test.js
@@ -2,7 +2,11 @@ var test = require('tape');
 var path = require('path');
 var geostats = require('../');
 
-geostats(fixture('simple.geojson'));
+geostats(fixture('simple.geojson'))
+// geostats(fixture('populations-plus.geojson'))
+  .catch(function (err) {
+    console.log(err);
+  });
 
 function fixture(fileName) {
   return path.join(__dirname, 'fixtures', fileName);
