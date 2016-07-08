e38fbcd90b59b37516a7c58e1a9a57da01778ca7
David Clark <dclark@mapbox.com>
2016-07-08 13:54:16 -0700

    Improvements and mapnik parallel implementation

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
 var path = require('path');
 var Promise = require('pinkie-promise');
+// var mapnikAnalyze = require('./lib/mapnik-analyze');
 var gdalAnalyze = require('./lib/gdal-analyze');
 var Stats = require('./lib/stats');
 
 function buildGeoStats(filePath) {
-  return new Promise(function () {
+  return new Promise(function (resolve) {
     var stats = new Stats(path.basename(filePath, path.extname(filePath)));
 
     if (!filePath) throw new Error('Filename required');
 
     var extension = path.extname(filePath);
 
-    switch (extension) {
-      case '.geojson':
-        return gdalAnalyze(stats, filePath);
-      default:
-        throw new Error('Invalid file type');
-    }
+    var analyze = (function () {
+      switch (extension) {
+        case '.geojson':
+          // return mapnikAnalyze(stats, filePath);
+          return gdalAnalyze(stats, filePath);
+        default:
+          throw new Error('Invalid file type');
+      }
+    }());
+
+    resolve(analyze.then(function (modifiedStats) {
+      return modifiedStats.toJSON();
+    }));
   });
 }
 
--- test/test.js
+++ test/test.js
@@ -2,10 +2,14 @@ var test = require('tape');
 var path = require('path');
 var geostats = require('../');
 
-geostats(fixture('simple.geojson'))
+var result = geostats(fixture('simple.geojson'));
+
 // geostats(fixture('populations-plus.geojson'))
+result.then(function (stats) {
+    console.log(JSON.stringify(stats, null, 2));
+  })
   .catch(function (err) {
-    console.log(err);
+    console.error(err.stack);
   });
 
 function fixture(fileName) {
