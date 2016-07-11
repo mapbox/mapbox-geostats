ecfaea9911f3d069a0bc7fc64a65fde795c21d72
David Clark <dclark@mapbox.com>
2016-07-11 16:08:31 -0700

    Compare using sloppySort

--- test/test.js
+++ test/test.js
@@ -2,6 +2,7 @@ var test = require('tap').test;
 var path = require('path');
 var fs = require('fs');
 var Promise = require('pinkie-promise');
+var sloppySort = require('./utils/sloppy-sort');
 var geostats = require('../');
 
 // var result = geostats(fixturePath('src/simple.geojson'));
@@ -10,76 +11,95 @@ var geostats = require('../');
 // var result = geostats(fixturePath('src/vectorgzip.mbtiles'));
 // var result = geostats(fixturePath('src/twolayers.mbtiles'));
 // var result = geostats(fixturePath('src/pngs.mbtiles')); /* raster I think */
-var result = geostats(fixturePath('src/simple.mbtiles'), {
-  attributes: ['astonishing'],
-});
-//
-result.then(function (stats) {
-  console.log(JSON.stringify(stats, null, 2))
-}).catch(logError);
+// var result = geostats(fixturePath('src/simple.mbtiles'), {
+//   attributes: ['astonishing'],
+// });
+// //
+// result.then(function (stats) {
+//   console.log(JSON.stringify(stats, null, 2))
+// }).catch(logError);
 
-// test('simple geojson', function (t) {
-//   Promise.all([
-//     geostats(fixturePath('src/simple.geojson')),
-//     getExpected('simple-geojson'),
-//   ]).then(function (output) {
-//     t.deepEqual(output[0], output[1]);
-//     t.end();
-//   }).catch(logError);
-// });
-//
-// test('simple mbtiles', function (t) {
-//   Promise.all([
-//     geostats(fixturePath('src/simple.mbtiles')),
-//     getExpected('simple-mbtiles'),
-//   ]).then(function (output) {
-//     t.deepEqual(output[0], output[1]);
-//     t.end();
-//   }).catch(logError);
-// });
-//
-// test('not-so-simple geojson', function (t) {
-//   Promise.all([
-//     geostats(fixturePath('src/populations-plus.geojson')),
-//     getExpected('populations-plus'),
-//   ]).then(function (output) {
-//     t.deepEqual(output[0], output[1]);
-//     t.end();
-//   }).catch(logError);
-// });
-//
-// test('shapefile', function (t) {
-//   Promise.all([
-//     geostats(fixturePath('src/ports/ports.shp')),
-//     getExpected('ports'),
-//   ]).then(function (output) {
-//     t.deepEqual(output[0], output[1]);
-//     t.end();
-//   }).catch(logError);
-// });
-//
-// test('gzipped mbtiles', function (t) {
-//   Promise.all([
-//     geostats(fixturePath('src/vectorgzip.mbtiles')),
-//     getExpected('vectorgzip'),
-//   ]).then(function (output) {
-//     t.deepEqual(output[0], output[1]);
-//     t.end();
-//   }).catch(logError);
-// });
-//
-// test('more gzipped mbtiles', function (t) {
-//   Promise.all([
-//     geostats(fixturePath('src/twolayers.mbtiles')),
-//     getExpected('twolayers'),
-//   ]).then(function (output) {
-//     t.deepEqual(output[0], output[1]);
-//     t.end();
-//   }).catch(logError);
-// });
-//
-// test('mbtiles with no features', function (t) {
-//   geostats(fixturePath('src/no-features.mbtiles')).then(function (output) {
+test('simple geojson', function (t) {
+  Promise.all([
+    geostats(fixturePath('src/simple.geojson')),
+    getExpected('simple-geojson'),
+  ]).then(function (output) {
+    t.deepEqual(sloppySort(output[0]), sloppySort(output[1]));
+    t.end();
+  }).catch(logError);
+});
+
+test('simple mbtiles', function (t) {
+  Promise.all([
+    geostats(fixturePath('src/simple.mbtiles')),
+    getExpected('simple-mbtiles'),
+  ]).then(function (output) {
+    t.deepEqual(sloppySort(output[0]), sloppySort(output[1]));
+    t.end();
+  }).catch(logError);
+});
+
+test('not-so-simple geojson', function (t) {
+  Promise.all([
+    geostats(fixturePath('src/populations-plus.geojson')),
+    getExpected('populations-plus'),
+  ]).then(function (output) {
+    t.deepEqual(output[0], output[1]);
+    t.end();
+  }).catch(logError);
+});
+
+test('shapefile', function (t) {
+  Promise.all([
+    geostats(fixturePath('src/ports/ports.shp')),
+    getExpected('ports'),
+  ]).then(function (output) {
+    t.deepEqual(output[0], output[1]);
+    t.end();
+  }).catch(logError);
+});
+
+test('gzipped mbtiles', function (t) {
+  Promise.all([
+    geostats(fixturePath('src/vectorgzip.mbtiles')),
+    getExpected('vectorgzip'),
+  ]).then(function (output) {
+    t.deepEqual(sloppySort(output[0]), sloppySort(output[1]));
+    t.end();
+  }).catch(logError);
+});
+
+test('more gzipped mbtiles', function (t) {
+  Promise.all([
+    geostats(fixturePath('src/twolayers.mbtiles')),
+    getExpected('twolayers'),
+  ]).then(function (output) {
+    t.deepEqual(sloppySort(output[0]), sloppySort(output[1]));
+    t.end();
+  }).catch(logError);
+});
+
+test('mbtiles with no features', function (t) {
+  geostats(fixturePath('src/no-features.mbtiles')).then(function (output) {
+    t.deepEqual(output, { layers: [] });
+    t.end();
+  }).catch(logError);
+});
+
+test('shapefile with no features', function (t) {
+  geostats(fixturePath('src/no-features/no-features.shp')).then(function (output) {
+    t.deepEqual(output, { layers: [{
+      attributeCount: 0,
+      attributes: [],
+      count: 0,
+      layer: 'no-features',
+    }] });
+    t.end();
+  }).catch(logError);
+});
+
+// test('geojson with no features', function (t) {
+//   geostats(fixturePath('src/no-features.geojson')).then(function (output) {
 //     t.deepEqual(output, { layers: [] });
 //     t.end();
 //   }).catch(logError);
---
+++ test/utils/sloppy-sort.js
@@ -0,0 +1,49 @@
+var _ = require('lodash');
+
+function sloppySortComparator(first, second) {
+  var isFirstObject = _.isPlainObject(first);
+  var isSecondObject = _.isPlainObject(second);
+  if (!isFirstObject && isSecondObject) return -1;
+  if (isFirstObject && !isSecondObject) return 1;
+  if (isFirstObject && isSecondObject) {
+    if (first.attribute < second.attribute) return -1;
+    if (first.attribute > second.attribute) return 1;
+    return 0;
+  }
+
+  var isFirstNumber = _.isNumber(first);
+  var isSecondNumber = _.isNumber(second);
+  if (!isFirstNumber && isSecondNumber) return -1;
+  if (isFirstNumber && !isSecondNumber) return 1;
+  if (isFirstNumber && isSecondNumber) {
+    if (first < second) return -1;
+    if (first > second) return 1;
+    return 0;
+  }
+
+  var isFirstNull = _.isNull(first);
+  var isSecondNull = _.isNull(second);
+  if (!isFirstNull && isSecondNull) return -1;
+  if (isFirstNull && !isSecondNull) return 1;
+  if (isFirstNull && isSecondNull) return 0;
+
+  if (String(first) < String(second)) return -1;
+  if (String(first) > String(second)) return 1;
+  return 0;
+}
+
+function sloppySortArray(arr) {
+  return arr.map(sloppySort).sort(sloppySortComparator);
+}
+
+function sloppySortObject(obj) {
+  return _.mapValues(obj, sloppySort);
+}
+
+function sloppySort(item) {
+  if (_.isPlainObject(item)) return sloppySortObject(item);
+  if (_.isArray(item)) return sloppySortArray(item);
+  return item;
+}
+
+module.exports = sloppySort;
