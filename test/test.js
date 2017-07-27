efa0a3fa70aa890e7e60a6dc4342cfccca319e67
Sam Matthews <matthews.sam@gmail.com>
2017-07-27 11:38:22 -0700

    validate json schema, grab tilestats from mbtiles (#33)
    * validate json schema for tilestats
    
    * check mbtiles for tilestats object before analyzing
    
    * davids review
    
    * LineString not Line
    
    * update schema to validate attribute arrays
    
    * update fixture, grab from json field
    
    * use previous version of mbtiles
    
    * update changelog [skip ci]

--- test/test.js
+++ test/test.js
@@ -6,6 +6,7 @@ const path = require('path');
 const fs = require('fs');
 const sloppySort = require('./utils/sloppy-sort');
 const geostats = require('../');
+const validator = require('../lib/validate-stats');
 
 function fixturePath(fileName) {
   return path.join(__dirname, 'fixtures', fileName);
@@ -407,4 +408,141 @@ test('MBTiles with two layers', t => {
     t.deepEqual(sloppySort(output[0]), sloppySort(output[1]), 'expected output');
     t.end();
   }).catch(t.threw);
+});
+
+test('MBTiles with tilestats metadata table returns as expected', t => {
+  Promise.all([
+    geostats(fixturePath('src/tilestats.mbtiles')),
+    getExpected('tilestats'),
+  ]).then((output) => {
+    t.deepEqual(sloppySort(output[0]), sloppySort(output[1]), 'expected output');
+    t.end();
+  }).catch(t.threw);
+});
+
+test('[validator] valid stats object', t => {
+  const stats = {
+    layerCount: 1,
+    layers: [
+      {
+        layer: 'test-layer',
+        count: 3,
+        geometry: 'Point',
+        attributeCount: 1,
+        attributes: [
+          {
+            attribute: 'test-attribute',
+            count: 3,
+            type: 'number',
+            values: [2, 5, 19],
+            min: 2,
+            max: 19,
+          },
+        ],
+      },
+    ],
+  };
+
+  t.ok(validator(stats));
+  t.end();
+});
+
+test('[validator] invalid stats object - no layers', t => {
+  const stats = {
+    layerCount: 1,
+  };
+
+  const results = validator(stats);
+  t.equal(results.length, 1, 'only one error');
+  t.equal(results[0], 'instance requires property "layers"', 'expected error message');
+  t.end();
+});
+
+test('[validator] invalid layer object - no layer name', t => {
+  const stats = {
+    layerCount: 1,
+    layers: [
+      {
+        count: 3,
+        geometry: 'Point',
+        attributeCount: 1,
+        attributes: [
+          {
+            attribute: 'test-attribute',
+            count: 3,
+            type: 'number',
+            values: [2, 5, 19],
+            min: 2,
+            max: 19,
+          },
+        ],
+      },
+    ],
+  };
+
+  const results = validator(stats);
+  t.equal(results.length, 1, 'only one error');
+  t.equal(results[0], 'instance.layers[0] requires property "layer"', 'expected error message');
+  t.end();
+});
+
+test('[validator] invalid layer object - no layer name', t => {
+  const stats = {
+    layerCount: 'wrong type',
+    layers: [
+      {
+        geometry: 'Point',
+        attributes: [
+          {
+            attribute: 'test-attribute',
+            type: 'number',
+            min: 2,
+            max: 19,
+          },
+        ],
+      },
+    ],
+  };
+
+  const expected = sloppySort([
+    'instance.layerCount is not of a type(s) number',
+    'instance.layers[0] requires property "count"',
+    'instance.layers[0].attributes[0] requires property "values"',
+    'instance.layers[0] requires property "layer"',
+    'instance.layers[0].attributes[0] requires property "count"',
+    'instance.layers[0] requires property "attributeCount"',
+  ]);
+
+  const results = validator(stats);
+  t.deepEqual(sloppySort(results), expected, 'expect lots of errors');
+  t.end();
+});
+
+test('[validator] invalid attributes contain mulitple types', t => {
+  const stats = {
+    layerCount: 1,
+    layers: [
+      {
+        layer: 'test-layer',
+        count: 3,
+        geometry: 'Point',
+        attributeCount: 1,
+        attributes: [
+          {
+            attribute: 'test-attribute',
+            count: 3,
+            type: 'number',
+            values: [2, 'five', null],
+            min: 2,
+            max: 19,
+          },
+        ],
+      },
+    ],
+  };
+
+  const results = validator(stats);
+  t.equal(results.length, 1, 'only one error');
+  t.equal(results[0], 'instance.layers[0].attributes[0].values is not any of [subschema 0],[subschema 1],[subschema 2],[subschema 3]', 'expected error message');
+  t.end();
 });
