3197998eb099b7f71d98b9ce754b8683d47e5714
Eli Fitch <eli.fitch.prof@gmail.com>
2019-05-28 16:42:37 -0400

    Fixing linting errors

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-var getFileType = require('./lib/get-file-type');
-var mapnikAnalyze = require('./lib/mapnik-analyze');
-var tileAnalyze = require('./lib/tile-analyze');
-var reportStats = require('./lib/report-stats');
-var Constants = require('./lib/constants');
+'use strict';
+const getFileType = require('./lib/get-file-type');
+const mapnikAnalyze = require('./lib/mapnik-analyze');
+const tileAnalyze = require('./lib/tile-analyze');
+const reportStats = require('./lib/report-stats');
+const Constants = require('./lib/constants');
 
 function buildGeoStats(filePath, options) {
   options = options || {};
@@ -18,14 +19,14 @@ function buildGeoStats(filePath, options) {
 
   // The number of unique values we'll count and record depends on
   // the number of attributes we might record
-  var divisor = (options.attributes)
+  const divisor = (options.attributes)
     ? Math.min(options.attributes.size, Constants.ATTRIBUTES_MAX_REPORT)
     : Constants.ATTRIBUTES_MAX_REPORT;
   options.maxValuesToCount = Math.floor(Constants.VALUES_MAX_COUNT / divisor);
   options.maxValuesToReport = Math.floor(Constants.VALUES_MAX_REPORT / divisor);
 
   return getFileType(filePath)
-    .then(function (fileType) {
+    .then(function(fileType) {
       if (fileType === Constants.FILETYPE_MBTILES) return tileAnalyze(filePath, options);
       return mapnikAnalyze(filePath, fileType, options);
     })
